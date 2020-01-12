dc8cc391ed2637a2ddb67d8cb4990d1f2dcacad1
smehlhoff <stevemehlhoff@gmail.com>
2020-01-12 12:45:03 -0800

    Add redshift to installed packages

--- fresh_install.sh
+++ fresh_install.sh
@@ -22,7 +22,7 @@ sudo apt update
 
 # install packages
 sudo apt install vim keepassx net-tools curl fish alacritty yadm bat git tmux htop fd-find 
-sudo apt install virtualenv python3-pip qpython3-virtualenv thunderbird
+sudo apt install virtualenv python3-pip qpython3-virtualenv thunderbird redshift
 
 # set default shell to fish
 chsh -s /usr/bin/fish
