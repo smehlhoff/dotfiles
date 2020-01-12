6c14e9b4f637bce6e22b83cc5bbcc557bbdaa9c8
smehlhoff <stevemehlhoff@gmail.com>
2020-01-12 00:38:39 -0800

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+set PATH $HOME/.cargo/bin $PATH
+
+alias wx="curl wttr.in"
+alias police="firefox https://www.broadcastify.com/listen/ctid/225"
+alias police2="firefox https://www.broadcastify.com/listen/feed/19186/web"
+alias cheat='echo -n "Search command: " && read command && curl cheat.sh/$command'
+
+alias reload="exec $SHELL -l"
+alias c="clear"
+alias find="fdfind"
+alias ls="exa -la"
+alias cat="bat"
+alias virtualenv="virtualenv -p python3"
