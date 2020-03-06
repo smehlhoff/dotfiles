57c280bf36f34531ccae22264a7dc1530a8c90b0
smehlhoff <stevemehlhoff@gmail.com>
2020-03-05 22:30:23 -0800

    Add reddit and twitch aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 set PATH $HOME/.cargo/bin $PATH
 
 alias wx="curl wttr.in"
+alias reddit="xdg-open https://www.reddit.com"
+alias twitch="xdg-open https://www.twitch.tv"
 alias police="xdg-open https://www.broadcastify.com/listen/ctid/225"
 alias police2="xdg-open https://www.broadcastify.com/listen/feed/19186/web"
 alias cheat='echo -n "Search command: " && read command && curl cheat.sh/$command'
