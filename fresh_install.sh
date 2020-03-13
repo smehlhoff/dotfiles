93ec8d36a7e9855a416ec818b16a1883226d3e00
smehlhoff <stevemehlhoff@gmail.com>
2020-03-13 11:17:38 -0700

    Add docker-compose

--- fresh_install.sh
+++ fresh_install.sh
@@ -48,6 +48,10 @@ sudo apt update
 sudo apt install -y docker-ce
 sudo systemctl status docker
 
+# install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # set default shell to fish
 chsh -s /usr/bin/fish
 
