7921a4ca45aba184c8df2d0534ba2d3c7d165446
smehlhoff <stevemehlhoff@gmail.com>
2020-03-13 17:50:49 -0700

    Add radar alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 set PATH $HOME/.cargo/bin $PATH
 
 alias wx="curl wttr.in"
+alias radar="xdg-open 'https://www.aviationweather.gov/radar/site?product=NCR&id=MUX'"
 alias reddit="xdg-open https://www.reddit.com"
 alias twitch="xdg-open https://www.twitch.tv"
 alias police="xdg-open https://www.broadcastify.com/listen/ctid/225"
