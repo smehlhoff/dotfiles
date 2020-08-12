d1715d8924aaa8509fdc6e80527a42f3940096aa
smehlhoff <stevemehlhoff@gmail.com>
2020-08-12 12:49:52 -0700

    Update URL for hackernews

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ set PATH $HOME/.cargo/bin $PATH
 alias wx="curl wttr.in"
 alias radar="xdg-open 'https://www.aviationweather.gov/radar/site?product=NCR&id=MUX'"
 alias reddit="xdg-open https://www.reddit.com"
-alias hn="xdg-open https://news.ycombinator.com/"
+alias hn="xdg-open https://news.ycombinator.com"
 alias twitch="xdg-open https://www.twitch.tv"
 alias chatty="java -jar ~/Downloads/Chatty/Chatty.jar"
 alias cheat='echo -n "Search command: " && read command && curl cheat.sh/$command'
@@ -19,6 +19,7 @@ alias ll="exa --all --long --grid"
 
 alias cat="bat"
 alias virtualenv="virtualenv -p python3"
+
 alias hibernate="systemctl suspend"
 
 # The next line updates PATH for the Google Cloud SDK.
