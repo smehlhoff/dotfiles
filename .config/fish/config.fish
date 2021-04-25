99708a9ff2ee9eeda0be3b2c4afba78c6e5bf720
smehlhoff <stevemehlhoff@gmail.com>
2021-04-24 21:34:33 -0700

    Add alias for cargo-update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,15 +4,15 @@ alias wx="curl wttr.in"
 alias ttv="xdg-open https://www.twitch.tv"
 alias reddit="xdg-open https://www.reddit.com"
 alias hn="xdg-open https://news.ycombinator.com"
-alias chatty="java -jar ~/Downloads/Chatty/Chatty.jar"
 alias radar="xdg-open 'https://radar.weather.gov/'"
+alias chatty="java -jar ~/Downloads/Chatty/Chatty.jar"
 
 alias cheat='echo -n "Search command: " && read command && curl cheat.sh/$command'
 
 alias cat="bat"
 alias find="fdfind"
-alias reload="exec $SHELL -l"
 alias virtualenv="virtualenv -p python3"
+alias cargo-update="cargo install-update -a"
 
 alias l="exa"
 alias la="exa --all"
@@ -20,6 +20,7 @@ alias lt="exa --tree"
 alias lg="exa --long --git"
 alias ll="exa --all --long --grid"
 
+alias reload="exec $SHELL -l"
 alias hibernate="systemctl suspend"
 
 # The next line updates PATH for the Google Cloud SDK.
