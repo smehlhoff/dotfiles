2b296dd88e6f013d7a3e6147193963651d678613
smehlhoff <977545+smehlhoff@users.noreply.github.com>
2024-07-13 23:20:43 -0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,13 @@
-set PATH $HOME/.cargo/bin $PATH
-
-alias wx="curl wttr.in"
-alias ttv="xdg-open https://www.twitch.tv"
-alias reddit="xdg-open https://www.reddit.com"
-alias hn="xdg-open https://news.ycombinator.com"
-alias radar="xdg-open 'https://radar.weather.gov/'"
-alias chatty="java -jar ~/Downloads/Chatty/Chatty.jar"
-
-alias cheat='echo -n "Search command: " && read command && curl cheat.sh/$command'
+alias cheat='read -l command; curl cheat.sh/$command'
 
 alias cat="bat"
-alias find="fdfind"
-alias virtualenv="virtualenv -p python3"
-alias cargo-update="cargo install-update -a"
+alias find="fd"
+alias zoxie="ls"
 
-alias l="exa"
-alias la="exa --all"
-alias lt="exa --tree"
-alias lg="exa --long --git"
-alias ll="exa --all --long --grid"
+alias l="eza"
+alias la="eza --all"
+alias lt="eza --tree"
+alias lg="eza --long --git"
+alias ll="eza --all --long --grid"
 
-alias reload="exec $SHELL -l"
-alias hibernate="systemctl suspend"
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/steve/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/steve/Downloads/google-cloud-sdk/path.fish.inc'; end
+alias reload="exec fish"
